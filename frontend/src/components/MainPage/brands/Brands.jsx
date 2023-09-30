dd2b883fafd681158c3c8aa87dfcd83eb8969210
Aniket <aniketdevmore1008@gmail.com>
2023-09-30 10:19:13 +0530

    resolve image error

--- frontend/src/components/MainPage/brands/Brands.jsx
+++ frontend/src/components/MainPage/brands/Brands.jsx
@@ -1,43 +1,28 @@
 import React from "react";
 import "./Brands.css";
+import syngenta from "../../../../public/static/images/Syngenta.png";
+import bayer from "../../../../public/static/images/Bayer.png";
+import multiplex from "../../../../public/static/images/Multiplex.png";
+import dhanuka from "../../../../public/static/images/Dhanuka.png";
+import tata from "../../../../public/static/images/Tata.png";
 
 const Brands = () => {
   return (
     <div id="mainBrand">
       <li className="brandLi">
-        <img
-          className="brandImg"
-          src="../../../../../public/static/images/Syngenta.png"
-          alt="syngenta"
-        />
+        <img className="brandImg" src={syngenta} alt="syngenta" />
       </li>
       <li className="brandLi">
-        <img
-          className="brandImg"
-          src="../../../../../public/static/images/Bayer.png"
-          alt="bayer"
-        />
+        <img className="brandImg" src={bayer} alt="bayer" />
       </li>
       <li className="brandLi">
-        <img
-          className="brandImg"
-          src="../../../../../public/static/images/Multiplex.png"
-          alt="KAK"
-        />
+        <img className="brandImg" src={multiplex} alt="KAK" />
       </li>
       <li className="brandLi">
-        <img
-          className="brandImg"
-          src="../../../../../public/static/images/Dhanuka.png"
-          alt="dhanuka"
-        />
+        <img className="brandImg" src={dhanuka} alt="dhanuka" />
       </li>
       <li className="brandLi">
-        <img
-          className="brandImg"
-          src="../../../../../public/static/images/Tata.png"
-          alt="tata"
-        />
+        <img className="brandImg" src={tata} alt="tata" />
       </li>
     </div>
   );
